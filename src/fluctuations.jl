498796dba9260e22de9684a6f7e685be16456126
timbode <lappet@protonmail.com>
2023-04-12 16:18:36 +0200

    Lyapunov exponents with adaptive ODE solver

---
+++ src/fluctuations.jl
@@ -0,0 +1,84 @@
+function magnetization(S::Matrix{<:Real}, h::Vector{<:Real}, J::Matrix{<:Real})
+    h + [sum([J[i, j] * S[3, j] for j in 1:size(S)[2]]) for i in 1:size(S)[2]]
+end
+
+"""
+    fluctuation_matrix(problem::Problem, S::Vector{<:Vector{<:Real}}, solutions::Vector{<:Real}, β::Real, γ::Real)
+
+Returns the Gaussian fluctuation matrix for a given point along the mean-field trajectories. 
+"""
+function fluctuation_matrix(problem::Problem, S::Matrix{<:Real}, solutions::Vector{<:Real}, β::Real, γ::Real)
+    num_qubits = problem.num_qubits
+    
+    A = zeros(ComplexF64, (num_qubits, num_qubits))
+    B = zeros(ComplexF64, (num_qubits, num_qubits))
+    τ_3 = diagm(vcat(ones(num_qubits), -ones(num_qubits)))
+
+    # helper function to construct A and B    
+    n_ij_pm = (idx, pm) -> solutions[idx] * S[1, idx] + pm * 1im * S[2, idx]
+
+    for i in 1:num_qubits
+        # we exclude a factor of 2 here because we symmetrize below
+        # signs in front of β, γ are reversed relative to the original paper
+        A[i, i] = -β * S[1, i] / (1 + solutions[i] * S[3, i]) - γ * solutions[i] * magnetization(S, problem.local_fields, problem.couplings)[i]
+        for j in i + 1:num_qubits
+            A[i, j] = γ * problem.couplings[i, j] * n_ij_pm(i, 1) * n_ij_pm(j, -1)
+            B[i, j] = γ * problem.couplings[i, j] * n_ij_pm(i, 1) * n_ij_pm(j,  1)        
+        end
+    end
+
+    # symmetrize
+    A += transpose(conj.(A))
+    B += transpose(B)
+
+    L = τ_3 * [A                   B;
+               transpose(conj.(B)) conj.(A)]
+
+    L
+end
+
+
+"""
+    evolve_fluctuations(problem::Problem, τ::Real, β::Vector{<:Real}, γ::Vector{<:Real})
+
+### Input
+- `problem::Problem`: A `Problem` instance defining the optimization problem.
+- `τ::Real`: The time-step of the considered annealing schedule.
+- `β::Vector{<:Real}`: The corresponding vector of QAOA driver parameters.
+- `γ::Vector{<:Real}`: The corresponding vector of QAOA problem parameters.    
+
+### Output
+- The Lyapunov exponents characterizing the dynamics of the Gaussian fluctuations.
+"""
+function evolve_fluctuations(problem::Problem, T_final::Real; rtol=1e-4, atol=1e-6)
+    num_qubits = problem.num_qubits    
+    
+    # evolution
+    schedule(t) = t / T_final
+    sol = evolve(problem.local_fields, problem.couplings, T_final, schedule, rtol=rtol, atol=atol)  
+    
+    # solution (rounded S_z values)
+    solution = S -> sign.([S[3, i] for i in 1:size(S)[2]])
+    solutions = solution(sol(T_final)) 
+    
+    lyapunov_exponent = [zeros(2num_qubits) for _ in 1:size(sol.t[2:end])[1]]
+    M = 1.0I(2num_qubits)
+    
+    Δts = map(x -> x[2] - x[1], zip(sol.t[1:end-1], sol.t[2:end]))
+    
+    for (k, t) in enumerate(sol.t[2:end])
+        L = fluctuation_matrix(problem, sol(t), solutions, 1 - schedule(t), schedule(t))        
+        omega_eig, omega_eigvec = eigen(L)
+        
+        M = omega_eigvec * diagm(exp.(-1im .* Δts[k] .* omega_eig)) * inv(omega_eigvec) * M
+
+        lyapunov_exponential_eig = eigvals(M * transpose(conj.(M)))
+
+        lyapunov_exponent_eig = log.((1.0 + 0.0im) * lyapunov_exponential_eig) ./ 2 .|> real
+        lyapunov_exponent_eig = sort(lyapunov_exponent_eig)
+
+        lyapunov_exponent[k] = lyapunov_exponent_eig        
+    end
+
+    lyapunov_exponent
+end
