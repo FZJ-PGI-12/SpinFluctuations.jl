8494e639d807abf439fb64d0691cd9f755a0db02
Tim <9047234+timbode@users.noreply.github.com>
2023-04-13 09:52:40 +0200

    5 use arpack for lyapunov exponents (#6)
    * using Arpack to maximal Lyapunov exponent
    
    * add utils
    
    * clean up
    
    ---------
    
    Co-authored-by: timbode <t.bode@Tims-MacBook-Pro.local>

---
+++ src/utils.jl
@@ -0,0 +1,36 @@
+function moving_average(vs, n)
+    [sum(@view vs[i:(i + n-1)]) / n for i in 1:(length(vs) - (n-1))]
+end
+
+function energies_and_probs(final_probs, annealing_problem)
+    L = annealing_problem.num_qubits
+    h = annealing_problem.local_fields
+    J = annealing_problem.couplings
+    
+    bitstrings = [string(i, base=2, pad=L) for i in 0:(2^L - 1)]
+    bitvals = [parse.(Int, [bitstring[j] for j in 1:L]) for bitstring in bitstrings]
+    spins = [1 .- 2s for s in bitvals]
+    
+    bistring_to_energy = Dict()
+    bistring_to_probs = Dict()
+    for (i, spin) in enumerate(spins)
+        E = sum([-h[l] * spin[l] for l in 1:L]) + sum([-J[i, j] * spin[i] * spin[j] for i in 1:L for j in (i+1):L])
+        bistring_to_energy[bitstrings[i]] = E
+        bistring_to_probs[bitstrings[i]] = final_probs[i]
+    end
+
+    energy_to_bistring = Dict((val, []) for (key, val) in bistring_to_energy)
+    for (key, val) in bistring_to_energy
+        push!(energy_to_bistring[val], key)
+    end
+    
+    energy_to_probs = Dict{Int, Float64}()
+    for (key, vals) in energy_to_bistring
+        energy_to_probs[key] = sum([bistring_to_probs[val] for val in vals])
+    end
+    
+    E = collect(keys(energy_to_probs)) |> sort
+    probs = [energy_to_probs[en] for en in E]
+    E = E .- minimum(E)
+    return (E, probs)
+end
