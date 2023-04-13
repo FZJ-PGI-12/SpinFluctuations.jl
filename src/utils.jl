739937ac14decaad5f3a183aed89bf9710483942
Tim <9047234+timbode@users.noreply.github.com>
2023-04-13 10:45:58 +0200

    add function that computes energy of most likely string (#8)
    Co-authored-by: timbode <t.bode@Tims-MacBook-Pro.local>

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,15 @@
+# function moving_average(vs, n)
+#     [sum(@view vs[i:(i + n-1)]) / n for i in 1:(length(vs) - (n-1))]
+# end
+
 function moving_average(vs, n)
-    [sum(@view vs[i:(i + n-1)]) / n for i in 1:(length(vs) - (n-1))]
+    res = similar(vs, length(vs) - (n-1))
+    @inbounds for i in 1:length(res)
+        res[i] = sum(@view vs[i:(i + n-1)]) / n
+    end
+    return res
 end
+
 
 function energies_and_probs(final_probs, annealing_problem)
     L = annealing_problem.num_qubits
@@ -33,4 +42,24 @@ function energies_and_probs(final_probs, annealing_problem)
     probs = [energy_to_probs[en] for en in E]
     E = E .- minimum(E)
     return (E, probs)
+end
+
+function energy_from_max_prob(final_probs, annealing_problem)
+    L = annealing_problem.num_qubits
+    h = annealing_problem.local_fields
+    J = annealing_problem.couplings
+
+    max_prob = maximum(final_probs)
+    idxs = findall(x -> x == maximum(final_probs), final_probs)
+    bitstrings = string.(idxs .- 1, base=2, pad=L)
+    bitvals = [parse.(Int, [bitstring[j] for j in 1:L]) for bitstring in bitstrings]
+    spins = [1 .- 2s for s in bitvals]
+
+    bistrings_to_energy = Dict()
+    for (i, spin) in enumerate(spins)
+        E = sum([-h[l] * spin[l] for l in 1:L]) + sum([-J[i, j] * spin[i] * spin[j] for i in 1:L for j in (i+1):L])
+        bistrings_to_energy[bitstrings[i]] = E
+    end
+    
+    return max_prob, bistrings_to_energy
 end
