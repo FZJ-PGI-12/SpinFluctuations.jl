703d7cc766bbb080457eea8c333a9e61f84d9cdb
timbode <lappet@protonmail.com>
2023-05-08 10:49:23 +0000

    update script

--- scripts/random_SK_instances.jl
+++ scripts/random_SK_instances.jl
@@ -1,12 +1,13 @@
 # script to generate 'hard' random SK instances
-using QAOA, AdaptiveQuantumAnnealing, LinearAlgebra, Arpack, Random, Distributions, Printf, HDF5, PyPlot
+using QAOA, AdaptiveQuantumAnnealing
+using LinearAlgebra, Arpack, Random, Distributions, Printf, HDF5, PyPlot
 
-PATH = "/Users/t.bode/.julia/dev/HardInstanceGenerator/"
+PATH = "/home/ubuntu/Archives/"
 N = 20
 
 folder_name = PATH * @sprintf("data/SK_model/N_%i/", N)
 
-for seed in 1:500
+for seed in [53, 55, 133, 219, 226, 238, 239, 257, 286, 288, 307, 318, 391, 414, 444, 473]
     printstyled("seed = ", seed, "\n", color=:green) 
     Random.seed!(seed)
     J = rand(Distributions.Normal(0, 1), N, N) ./ sqrt(N) 
@@ -16,32 +17,32 @@ for seed in 1:500
 
     mf_problem = Problem(0, J_mat)
 
-    # # do mean-field computations
+    # do mean-field computations
 
-    # h5write(folder_name * @sprintf("random_SK_instance_N_%i_seed_%i.h5", N, seed), "J", J_mat)
+    h5write(folder_name * @sprintf("random_SK_instance_N_%i_seed_%i.h5", N, seed), "J", J_mat)
 
-    # T_final = 30000.
-    # tol = 1e-9
-    # adap_times, solution, lyapunov_exponent = maximal_lyapunov_exponent(mf_problem, T_final, rtol=1e2*tol, atol=tol)
+    T_final = 30000.
+    tol = 1e-11
+    adap_times, solution, lyapunov_exponent = maximal_lyapunov_exponent(mf_problem, T_final, rtol=1e2*tol, atol=tol)
 
-    # h5write(folder_name * @sprintf("random_SK_instance_N_%i_seed_%i.h5", N, seed), "times", adap_times)
-    # h5write(folder_name * @sprintf("random_SK_instance_N_%i_seed_%i.h5", N, seed), "mean_field_solution", solution)
-    # h5write(folder_name * @sprintf("random_SK_instance_N_%i_seed_%i.h5", N, seed), "lyapunov_exponent", lyapunov_exponent)
+    h5write(folder_name * @sprintf("random_SK_instance_N_%i_seed_%i.h5", N, seed), "times", adap_times)
+    h5write(folder_name * @sprintf("random_SK_instance_N_%i_seed_%i.h5", N, seed), "mean_field_solution", solution)
+    h5write(folder_name * @sprintf("random_SK_instance_N_%i_seed_%i.h5", N, seed), "lyapunov_exponent", lyapunov_exponent)
 
-    # figure(figsize=(4, 3))
-    # subplot(111)
-    # plot(adap_times[2:end] ./ T_final, lyapunov_exponent, "-C0", label="λ")
-    # xlim(0, 1)
-    # ylim(0, )
-    # xlabel("s")
-    # legend(frameon=false)
-    # tight_layout()
-    # savefig(PATH * @sprintf("plots/SK_model/N_%i/", N) * @sprintf("random_SK_instance_N_%i_seed_%i.pdf", N, seed))
-    # close()
+    figure(figsize=(4, 3))
+    subplot(111)
+    plot(adap_times[2:end] ./ T_final, lyapunov_exponent, "-C0", label="λ")
+    xlim(0, 1)
+    ylim(0, )
+    xlabel("s")
+    legend(frameon=false)
+    tight_layout()
+    savefig(PATH * @sprintf("plots/SK_model/N_%i/", N) * @sprintf("random_SK_instance_N_%i_seed_%i.pdf", N, seed))
+    close()
 
     # get spectrum
     exact_times = range(0, 1, 26)
-    eigeninfo = map(s -> (eigs(-AdaptiveQuantumAnnealing.hamiltonian(1 - s, s, mf_problem.local_fields, mf_problem.couplings), nev=8, which=:LM, maxiter=300)), exact_times)
+    eigeninfo = map(s -> (eigs(-AdaptiveQuantumAnnealing.hamiltonian(1 - s, s, mf_problem.local_fields, mf_problem.couplings), nev=8, which=:LM, maxiter=2000)), exact_times)
     λ = [vals[1] for vals in eigeninfo]
     λ = reduce(hcat, λ)
     final_eigvecs = eigeninfo[end][2]
