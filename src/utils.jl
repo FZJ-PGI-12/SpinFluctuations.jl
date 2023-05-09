4074a69f8b5a426de44177fc83edf0ad9dbb0e87
Tim <9047234+timbode@users.noreply.github.com>
2023-05-09 15:34:30 +0200

    22 investigate hard sk instances (#23)
    * add bitstring and energy dicts
    
    * update deps
    
    * add notebook
    
    * update deps
    
    * good instance
    
    ---------
    
    Co-authored-by: timbode <t.bode@Tims-MacBook-Pro.local>
    Co-authored-by: timbode <t.bode@tims-mbp.localdomain>

--- src/utils.jl
+++ src/utils.jl
@@ -20,22 +20,22 @@ function energies_and_probs(final_probs, annealing_problem)
     bitvals = [parse.(Int, [bitstring[j] for j in 1:L]) for bitstring in bitstrings]
     spins = [1 .- 2s for s in bitvals]
     
-    bistring_to_energy = Dict()
-    bistring_to_probs = Dict()
+    bitstring_to_energy = Dict()
+    bitstring_to_probs = Dict()
     for (i, spin) in enumerate(spins)
         E = sum([-h[l] * spin[l] for l in 1:L]) + sum([-J[i, j] * spin[i] * spin[j] for i in 1:L for j in (i+1):L])
-        bistring_to_energy[bitstrings[i]] = E
-        bistring_to_probs[bitstrings[i]] = final_probs[i]
+        bitstring_to_energy[bitstrings[i]] = E
+        bitstring_to_probs[bitstrings[i]] = final_probs[i]
     end
 
-    energy_to_bistring = Dict((val, []) for (key, val) in bistring_to_energy)
-    for (key, val) in bistring_to_energy
+    energy_to_bistring = Dict((val, []) for (key, val) in bitstring_to_energy)
+    for (key, val) in bitstring_to_energy
         push!(energy_to_bistring[val], key)
     end
     
     energy_to_probs = Dict{Float64, Float64}()
     for (key, vals) in energy_to_bistring
-        energy_to_probs[key] = sum([bistring_to_probs[val] for val in vals])
+        energy_to_probs[key] = sum([bitstring_to_probs[val] for val in vals])
     end
     
     E = collect(keys(energy_to_probs)) |> sort
@@ -44,14 +44,15 @@ function energies_and_probs(final_probs, annealing_problem)
     return (E, probs)
 end
 
+
 function energy_from_max_prob(final_probs, annealing_problem)
     L = annealing_problem.num_qubits
     h = annealing_problem.local_fields
     J = annealing_problem.couplings
 
     max_prob = maximum(final_probs)
-    idxs = findall(x -> x == maximum(final_probs), final_probs)
-    bitstrings = string.(idxs .- 1, base=2, pad=L) .|> reverse 
+    idxs = findfirst(x -> x == max_prob, final_probs)
+    bitstrings = [string.(idxs .- 1, base=2, pad=L) .|> reverse]
     bitvals = [parse.(Int, [bitstring[j] for j in 1:L]) for bitstring in bitstrings]
     spins = [1 .- 2s for s in bitvals]
 
@@ -62,4 +63,29 @@ function energy_from_max_prob(final_probs, annealing_problem)
     end
     
     return max_prob, bistrings_to_energy
+end
+
+
+function energies_and_bitstrings(annealing_problem)
+    L = annealing_problem.num_qubits
+    h = annealing_problem.local_fields
+    J = annealing_problem.couplings
+    
+    bitstrings = [string(i, base=2, pad=L) |> reverse for i in 0:(2^L - 1)]
+    bitvals = [parse.(Int, [bitstring[j] for j in 1:L]) for bitstring in bitstrings]
+    spins = [1 .- 2s for s in bitvals]
+    
+    bitstring_to_energy = Dict()
+    bitstring_to_probs = Dict()
+    for (i, spin) in enumerate(spins)
+        E = sum([-h[l] * spin[l] for l in 1:L]) + sum([-J[i, j] * spin[i] * spin[j] for i in 1:L for j in (i+1):L])
+        bitstring_to_energy[bitstrings[i]] = E
+    end
+
+    energy_to_bistring = Dict((val, []) for (key, val) in bitstring_to_energy)
+    for (key, val) in bitstring_to_energy
+        push!(energy_to_bistring[val], key)
+    end
+    
+    return (bitstring_to_energy, energy_to_bistring)
 end
