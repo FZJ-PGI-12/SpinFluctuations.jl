f2b740da99f4d7ba0a2074fff6e0bc2dde3dcdeb
Tim <9047234+timbode@users.noreply.github.com>
2023-09-11 12:04:35 +0200

    51 solve for spectral function directly (#52)
    * add evolve_spectral_function
    
    * closes #51
    
    ---------
    
    Co-authored-by: timbode <t.bode@tims-mbp-2.speedport_w_724v_typ_a_05011603_06_003>

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,29 @@
+# spectrum as a function of T for given spin index
+function spectrum_vs_T(idx, spectral_sols)
+    all_ωs = []
+    all_specs = []
+    for (k, spectral_sol) in enumerate(spectral_sols)
+        ωs, spec = spectrum(spectral_sol, idx)
+        push!(all_ωs, ωs)
+        push!(all_specs, spec)    
+    end
+    (all_ωs, all_specs)
+end    
+
+# spectrum for fixed diagonal T and spin index idx
+function spectrum(spectral_sol, idx)
+    τ_final = spectral_sol.t[end]
+    npts = 2length(spectral_sol.t)
+    τ_range = range(0, τ_final, npts+1)
+    ωs = [(ω - 1 - npts/2)/τ_final for ω in 1:npts+1]
+
+    # extrapolate to uniform grid and perform FFT
+    fft_f = fftshift(fft(map(τ -> spectral_sol(τ)[idx, idx], τ_range)))
+    spec = abs.(fft_f) ./ maximum(abs.(fft_f))
+
+    ωs, spec
+end
+
 function spectrum(ρ_T::Vector{ComplexF64}, lyapunov_parameters::LyapunovParameters)
     @unpack_LyapunovParameters lyapunov_parameters
     @assert size(ρ_T, 1) == npts+1
