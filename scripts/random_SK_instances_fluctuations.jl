1171c68f67e7948df1ecedc6babb8049af87b549
timbode <lappet@protonmail.com>
2023-11-29 09:50:24 +0000

    added finer fluctuations also for large gaps at N=9

--- scripts/random_SK_instances_fluctuations.jl
+++ scripts/random_SK_instances_fluctuations.jl
@@ -24,21 +24,21 @@ PATH = "/home/ubuntu/Archives/"
 N = 9
 pattern = r"random_SK_instance_N_9_seed_(\d+)\.h5"
 
-subdir = "small_gaps"
-# subdir = "large_gaps"
+# subdir = "small_gaps"
+subdir = "large_gaps"
 folder_name = PATH * @sprintf("data/SK_model/N_%i/%s/", N, subdir)
 instance_names = readdir(folder_name)
 filter!(x -> !occursin("results", x), instance_names)
 filter!(x -> !occursin("undecided", x), instance_names)
 
-missing_seeds = ["9943"]
+# missing_seeds = ["9943"]
 
-# for (k, instance_name) in enumerate(instance_names[loop_var:loop_var+99])
-for (k, instance_name) in enumerate(instance_names)
+for (k, instance_name) in enumerate(instance_names[loop_var:loop_var+24])
+# for (k, instance_name) in enumerate(instance_names)
     seed = match(pattern, instance_name)[1]
-    if seed ∉ missing_seeds
-        continue
-    end
+    # if seed ∉ missing_seeds
+    #     continue
+    # end
     printstyled(Dates.format(now(), "HH:MM") * "|> ", instance_name, @sprintf(" is loop number %i", k), "\n", color=:yellow)
 
     # λ = h5read(folder_name * instance_name, "exact_ARPACK_LM_eigvals")
