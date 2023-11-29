be6c14c4f3ed3840d4291a198a3f35d995d8a0c5
timbode <lappet@protonmail.com>
2023-11-29 08:32:55 +0000

    N=9 data set complete

--- scripts/random_SK_instances_fluctuations.jl
+++ scripts/random_SK_instances_fluctuations.jl
@@ -3,6 +3,7 @@ using LinearAlgebra, Arpack, Random, Distributions, Printf, HDF5
 using Dates
 using PyPlot
 
+loop_var = parse(Int, ARGS[1])
 PATH = "/home/ubuntu/Archives/"
 
 # N = 19
@@ -27,24 +28,24 @@ subdir = "small_gaps"
 # subdir = "large_gaps"
 folder_name = PATH * @sprintf("data/SK_model/N_%i/%s/", N, subdir)
 instance_names = readdir(folder_name)
+filter!(x -> !occursin("results", x), instance_names)
+filter!(x -> !occursin("undecided", x), instance_names)
 
-loop_var = parse(Int, ARGS[1])
+missing_seeds = ["9943"]
 
-missing_seeds = ["13873", "21890", "29855", "29900"]
-
-for instance_name in instance_names#[loop_var:loop_var]
+# for (k, instance_name) in enumerate(instance_names[loop_var:loop_var+99])
+for (k, instance_name) in enumerate(instance_names)
     seed = match(pattern, instance_name)[1]
     if seed ∉ missing_seeds
         continue
     end
-    printstyled(Dates.format(now(), "HH:MM") * ": ", instance_name, "\n", color=:blue)    
+    printstyled(Dates.format(now(), "HH:MM") * "|> ", instance_name, @sprintf(" is loop number %i", k), "\n", color=:yellow)
 
-    λ = h5read(folder_name * instance_name, "exact_ARPACK_LM_eigvals")
-
-    gap = λ[2, :] .- λ[1, :];
-    exact_times = range(0, 1, 33)
-    gaploc = exact_times[findfirst(x -> x == minimum(gap), gap)] 
-    printstyled("\t", Dates.format(now(), "HH:MM") * ": Gap is located around ", gaploc, "\n", color=:blue)
+    # λ = h5read(folder_name * instance_name, "exact_ARPACK_LM_eigvals")
+    # gap = λ[2, :] .- λ[1, :];
+    # exact_times = range(0, 1, 33)
+    # gaploc = exact_times[findfirst(x -> x == minimum(gap), gap)] 
+    # printstyled("\t", Dates.format(now(), "HH:MM") * ": Gap is located around ", gaploc, "\n", color=:blue)
 
     couplings = h5read(folder_name * instance_name, "J")
     mf_problem = Problem(0, couplings)
@@ -52,7 +53,7 @@ for instance_name in instance_names#[loop_var:loop_var]
     # write to results file
     instance_name = "results_" * instance_name
 
-    T_final = 32000.
+    T_final = 32768.
     tol = 1e-8
 
     # Bogoliubov spectrum
@@ -62,10 +63,10 @@ for instance_name in instance_names#[loop_var:loop_var]
     bogo_spec = sort(bogo_spec .|> real, dims=1)
 
     h5write(folder_name * instance_name, @sprintf("bogoliubov_spectrum_T_final_%.0f_tol_1e%.0f_npts_%i", T_final, log10(tol), npts_bogo), bogo_spec)
-    printstyled("\t", Dates.format(now(), "HH:MM") * ": Bogoliubov spectrum done.", "\n", color=:green)
+    printstyled("\t", Dates.format(now(), "HH:MM") * "|> Bogoliubov spectrum done.", "\n", color=:green)
 
     # statistical Green function
-    npts = 2048
+    npts = 4096
     coarse_times = range(0, 1, npts + 1)
     lyapunov_parameters = LyapunovParameters(T_final, npts, tol, tol)
     mf_sol, stat_GF = statistical_green_function(mf_problem, lyapunov_parameters)
@@ -74,7 +75,7 @@ for instance_name in instance_names#[loop_var:loop_var]
     all_flucs = reduce(hcat, map(flucs, 1:npts+1))
 
     h5write(folder_name * instance_name, @sprintf("fluctuations_T_final_%.0f_tol_1e%.0f_npts_%i", T_final, log10(tol), npts), all_flucs)
-    printstyled("\t", Dates.format(now(), "HH:MM") * ": Fluctuations done.", "\n", color=:green)
+    printstyled("\t", Dates.format(now(), "HH:MM") * "|> Fluctuations done.", "\n", color=:green)
 end
 
 
