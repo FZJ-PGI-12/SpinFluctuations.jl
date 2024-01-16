077effb93f6d49103db1071dae46c159d5ddabd8
timbode <lappet@protonmail.com>
2024-01-16 11:02:10 +0000

    mean-field fluctuations for 150 N+1=19 instances complete

--- scripts/random_SK_instances_fluctuations.jl
+++ scripts/random_SK_instances_fluctuations.jl
@@ -6,8 +6,8 @@ using PyPlot
 loop_var = parse(Int, ARGS[1])
 PATH = "/home/ubuntu/Archives/"
 
-# N = 19
-# pattern = r"random_SK_instance_N_19_seed_(\d+)\.h5"
+N = 19
+pattern = r"random_SK_instance_N_19_seed_(\d+)\.h5"
 
 # N = 17
 # pattern = r"random_SK_instance_N_17_seed_(\d+)\.h5"
@@ -21,36 +21,33 @@ PATH = "/home/ubuntu/Archives/"
 # N = 11 
 # pattern = r"random_SK_instance_N_11_seed_(\d+)\.h5"
 
-N = 9
-pattern = r"random_SK_instance_N_9_seed_(\d+)\.h5"
+# N = 9
+# pattern = r"random_SK_instance_N_9_seed_(\d+)\.h5"
 
 subdir = "small_gaps"
+# missing_seeds = string.([1302, 1315, 1336, 1359, 1400, 1402, 1430, 1456, 1478, 1493, 1498, 1509, 1515, 1521, 1523, 1542, 1557, 1603, 1621, 1624, 1633, 1656, 1659, 1666, 1683, 1688, 1690, 1691, 1695, 1726, 1750, 1760])
+
 # subdir = "large_gaps"
+# missing_seeds = string.([1367, 1369, 1379, 1416, 1447, 1552])
+
 folder_name = PATH * @sprintf("data/SK_model/N_%i/%s/", N, subdir)
 instance_names = readdir(folder_name)
 filter!(x -> !occursin("results", x), instance_names)
 filter!(x -> !occursin("undecided", x), instance_names)
 filter!(x -> !occursin("frustrated", x), instance_names)
+filter!(x -> !occursin("main_df", x), instance_names)
 
-# missing_seeds = ["9943"]
-
-for (k, instance_name) in enumerate(instance_names[loop_var:loop_var+99])
+for (k, instance_name) in enumerate(instance_names[loop_var:loop_var+9])
 # for (k, instance_name) in enumerate(instance_names)
-#     seed = match(pattern, instance_name)[1]
-#     if seed ∉ missing_seeds
-#         continue
-#     end
+    # seed = match(pattern, instance_name)[1]
+    # if seed ∉ missing_seeds
+    #     continue
+    # end
     try
-        h5read(folder_name * "results_" * instance_name, @sprintf("fluctuations_T_final_%.0f_tol_1e%.0f_npts_%i", 65536., log10(1e-8), 8192))
+        h5read(folder_name * "results_" * instance_name, @sprintf("fluctuations_T_final_%.0f_tol_1e%.0f_npts_%i", 32000., log10(1e-8), 2048))
         printstyled(Dates.format(now(), "HH:MM") * " |> ", instance_name, " exists\n", color=:green)
     catch
         printstyled(Dates.format(now(), "HH:MM") * " |> ", instance_name, @sprintf(" is loop number %i", k), "\n", color=:white)
-
-        # λ = h5read(folder_name * instance_name, "exact_ARPACK_LM_eigvals")
-        # gap = λ[2, :] .- λ[1, :];
-        # exact_times = range(0, 1, 33)
-        # gaploc = exact_times[findfirst(x -> x == minimum(gap), gap)] 
-        # printstyled("\t", Dates.format(now(), "HH:MM") * ": Gap is located around ", gaploc, "\n", color=:blue)
 
         couplings = h5read(folder_name * instance_name, "J")
         mf_problem = Problem(0, couplings)
@@ -58,24 +55,24 @@ for (k, instance_name) in enumerate(instance_names[loop_var:loop_var+99])
         # write to results file
         instance_name = "results_" * instance_name
 
-        # T_final = 32000.
+        T_final = 32000.
         # T_final = 32768.
-        T_final = 65536.
+        # T_final = 65536.
         tol = 1e-8
 
-        # # Bogoliubov spectrum
-        # npts_bogo = 32
-        # bogo_spec = bogoliubov_spectrum(mf_problem, LyapunovParameters(T_final, npts_bogo, tol, tol))
-        # bogo_spec = reduce(hcat, bogo_spec)
-        # bogo_spec = sort(bogo_spec .|> real, dims=1)
+        # Bogoliubov spectrum
+        npts_bogo = 32
+        bogo_spec = bogoliubov_spectrum(mf_problem, LyapunovParameters(T_final, npts_bogo, tol, tol))
+        bogo_spec = reduce(hcat, bogo_spec)
+        bogo_spec = sort(bogo_spec .|> real, dims=1)
 
-        # h5write(folder_name * instance_name, @sprintf("bogoliubov_spectrum_T_final_%.0f_tol_1e%.0f_npts_%i", T_final, log10(tol), npts_bogo), bogo_spec)
-        # printstyled("\t", Dates.format(now(), "HH:MM") * "|> Bogoliubov spectrum done.", "\n", color=:green)
+        h5write(folder_name * instance_name, @sprintf("bogoliubov_spectrum_T_final_%.0f_tol_1e%.0f_npts_%i", T_final, log10(tol), npts_bogo), bogo_spec)
+        printstyled("\t", Dates.format(now(), "HH:MM") * "|> Bogoliubov spectrum done.", "\n", color=:green)
 
         # statistical Green function
-        # npts = 2048
+        npts = 2048
         # npts = 4096
-        npts = 8192
+        # npts = 8192
         coarse_times = range(0, 1, npts + 1)
         lyapunov_parameters = LyapunovParameters(T_final, npts, tol, tol)
         mf_sol, stat_GF = statistical_green_function(mf_problem, lyapunov_parameters)
--- scripts/random_SK_instances_mean_field.jl
+++ scripts/random_SK_instances_mean_field.jl
@@ -6,8 +6,8 @@ using PyPlot
 loop_var = parse(Int, ARGS[1])
 PATH = "/home/ubuntu/Archives/"
 
-# N = 19
-# pattern = r"random_SK_instance_N_19_seed_(\d+)\.h5"
+N = 19
+pattern = r"random_SK_instance_N_19_seed_(\d+)\.h5"
 
 # N = 17
 # pattern = r"random_SK_instance_N_17_seed_(\d+)\.h5"
@@ -21,49 +21,58 @@ PATH = "/home/ubuntu/Archives/"
 # N = 11 
 # pattern = r"random_SK_instance_N_11_seed_(\d+)\.h5"
 
-N = 9
-pattern = r"random_SK_instance_N_9_seed_(\d+)\.h5"
+# N = 9
+# pattern = r"random_SK_instance_N_9_seed_(\d+)\.h5"
+
 
 subdir = "small_gaps"
+# missing_seeds = string.([1302, 1315, 1336, 1359, 1400, 1402, 1430, 1456, 1478, 1493, 1498, 1509, 1515, 1521, 1523, 1542, 1557, 1603, 1621, 1624, 1633, 1656, 1659, 1666, 1683, 1688, 1690, 1691, 1695, 1726, 1750, 1760])
+
 # subdir = "large_gaps"
+# missing_seeds = string.([1367, 1369, 1379, 1416, 1447, 1552])
+
 folder_name = PATH * @sprintf("data/SK_model/N_%i/%s/", N, subdir)
 instance_names = readdir(folder_name)
 filter!(x -> !occursin("results", x), instance_names)
 filter!(x -> !occursin("undecided", x), instance_names)
+filter!(x -> !occursin("frustrated", x), instance_names)
+filter!(x -> !occursin("main_df", x), instance_names)
 
-missing_seeds = ["9943"]
+T_final = 32768.
+tol = 1e-6
 
-# for (k, instance_name) in enumerate(instance_names[loop_var:loop_var+24])
-for (k, instance_name) in enumerate(instance_names)
-    seed = match(pattern, instance_name)[1]
-    if seed ∉ missing_seeds
-        continue
+for (k, instance_name) in enumerate(instance_names[loop_var:loop_var+9])
+# for (k, instance_name) in enumerate(instance_names)
+    # seed = match(pattern, instance_name)[1]
+    # if seed ∉ missing_seeds
+    #     continue
+    # end
+    try
+        h5read(folder_name * "results_" * instance_name, @sprintf("mean_field_sol_T_final_%.0f_tol_1e%.0f", T_final, log10(tol)))
+    catch
+        printstyled(Dates.format(now(), "HH:MM") * ": ", instance_name, @sprintf(" is loop number %i", k), "\n", color=:blue)
+        printstyled("\t", Dates.format(now(), "HH:MM") * ": Getting mean-field...", "\n", color=:white)
+
+        couplings = h5read(folder_name * instance_name, "J")
+        mf_problem = Problem(0, couplings)
+
+        schedule(t) = t / T_final
+        sol = evolve_mean_field(mf_problem.local_fields, mf_problem.couplings, T_final, schedule, rtol=1e2tol, atol=tol) 
+        
+        # get mean-field solution
+        solution = S -> sign.([S[3, i] for i in 1:size(S)[2]])
+        mf_sol = solution(sol(T_final)) 
+
+        # write to results file
+        instance_name = "results_" * instance_name
+
+        writable_data = zeros(length(sol.u), size(sol.u[1])...)
+        for i in 1:length(sol.u)
+            writable_data[i, :, :] .= sol.u[i]
+        end
+        h5write(folder_name * instance_name, @sprintf("mean_field_sol_T_final_%.0f_tol_1e%.0f", T_final, log10(tol)), mf_sol)
+        h5write(folder_name * instance_name, @sprintf("mean_field_T_final_%.0f_tol_1e%.0f/times", T_final, log10(tol)), sol.t)
+        h5write(folder_name * instance_name, @sprintf("mean_field_T_final_%.0f_tol_1e%.0f/trajectories", T_final, log10(tol)), writable_data)
+        printstyled("\t", Dates.format(now(), "HH:MM") * ": Mean-field trajectories done.", "\n", color=:green)
     end
-    printstyled(Dates.format(now(), "HH:MM") * ": ", instance_name, @sprintf(" is loop number %i", k), "\n", color=:blue)
-    printstyled("\t", Dates.format(now(), "HH:MM") * ": Getting mean-field...", "\n", color=:white)
-
-    couplings = h5read(folder_name * instance_name, "J")
-    mf_problem = Problem(0, couplings)
-
-    T_final = 32768.
-    tol = 1e-6
-
-    schedule(t) = t / T_final
-    sol = evolve_mean_field(mf_problem.local_fields, mf_problem.couplings, T_final, schedule, rtol=1e2tol, atol=tol) 
-    
-    # get mean-field solution
-    solution = S -> sign.([S[3, i] for i in 1:size(S)[2]])
-    mf_sol = solution(sol(T_final)) 
-
-    # write to results file
-    instance_name = "results_" * instance_name
-
-    writable_data = zeros(length(sol.u), size(sol.u[1])...)
-    for i in 1:length(sol.u)
-        writable_data[i, :, :] .= sol.u[i]
-    end
-    h5write(folder_name * instance_name, @sprintf("mean_field_sol_T_final_%.0f_tol_1e%.0f", T_final, log10(tol)), mf_sol)
-    h5write(folder_name * instance_name, @sprintf("mean_field_T_final_%.0f_tol_1e%.0f/times", T_final, log10(tol)), sol.t)
-    h5write(folder_name * instance_name, @sprintf("mean_field_T_final_%.0f_tol_1e%.0f/trajectories", T_final, log10(tol)), writable_data)
-    printstyled("\t", Dates.format(now(), "HH:MM") * ": Mean-field trajectories done.", "\n", color=:green)
 end
